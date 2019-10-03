112fdff94e8e99b9e780293803c14a638122d0a3
tmam101 <thomaspgoss98@gmail.com>
2019-10-03 19:33:47 -0400

    Correct request

--- Project/Backend/database.js
+++ Project/Backend/database.js
@@ -79,7 +79,7 @@ const network = require('./network.js')
 async function distanceBetweenTwoPoints(origin, destination) {
   var response = await network.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=700+Bolinwood+Dr+Chapel+Hill+NC&destinations=209+South+Rd+Chapel+Hill+NC&key=" + googleAPIKEY)
   console.log(response);
-  console.log(response.elements)
+  console.log(response.rows[0].elements)
 }
 
 exports.distanceBetweenTwoPoints = distanceBetweenTwoPoints;

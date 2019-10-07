39f6fa7ce6e7f899f77690e6a0c49361ae4769f2
Ethan Bateman <jetfire725@hotmail.com>
2019-10-07 16:20:27 -0400

    more tests2

--- Project/Backend/network.js
+++ Project/Backend/network.js
@@ -80,7 +80,7 @@ async function respond2(response, value){
 }
 
 async function getdata (){
-  let value = database.getAmenityByID(JSON.parse(this.req.chunks[0]).id);
+  let value = database.getAmenityByID(1);
   console.log(value);
   respond2(this.res, value);
 }
@@ -139,44 +139,8 @@ function respondToPost() {
   index.respondToPost(this.req, this.res);
 }
 
-//MARK: HTTPS FUNCTIONS
-async function get(url) {
-  var promise = new Promise(function(resolve, reject) {
-    HTTPS.get(url, (resp) => {
-      let data = '';
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
-      resp.on('end', () => {
-        // Do the actual work
-        resolve(JSON.parse(data))
-      });
-    }).on("error", (err) => {
-      console.log("Error: " + err.message);
-      reject()
-    });
-  });
-  return promise
-}
 
-function post(options, body) {
-  var botReq;
-  botReq = HTTPS.request(options, function(res) {
-    // console.log(res)
-    if(res.statusCode != 202) {
-      console.log('rejecting bad status code ' + res.statusCode);
-    } else {
-      // console.log('good status code')
-    }
-  });
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
-}
+
 
 exports.startServer = startServer;
 exports.get = get;

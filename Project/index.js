98e82a9de02a5841268e8f90980b6fadc7ed3a5c
tmam101 <thomaspgoss98@gmail.com>
2019-10-08 14:11:09 -0400

    Run refresh on startup

--- Project/index.js
+++ Project/index.js
@@ -3,16 +3,17 @@ const 	server		= require('./Backend/server.js')
 const		network 	= require('./Backend/network.js')
 const 	database  = require('./Backend/database.js')
 const 	thisURL		= "salon-on-backend.herokuapp.com"
-
+const 	testURL 	= "salon-on-thomas.herokuapp.com/"
 //SERVER & SETUP
 async function start() {
 	await server.startServer();
 	//setupRefresh();
 	await database.connect();
+	network.get(testURL)
 
 	//REFRESH -- CHANGE TO http://localhost:3000/refresh FOR LOCAL TESTING
 	setInterval(function() {
-		network.get("salon-on-thomas.herokuapp.com/");
+		network.get(testURL);
 	}, 300000); // every 5 minutes (300000)
 
 	//TODO Test

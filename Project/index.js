ebb8660a56ccffcf7754dc77991dac4c60d3cbe6
tmam101 <thomaspgoss98@gmail.com>
2019-10-14 00:48:27 -0400

    Update index.js

--- Project/index.js
+++ Project/index.js
@@ -3,7 +3,7 @@ const 	server		= require('./Backend/server.js')
 const		network 	= require('./Backend/network.js')
 const 	database  = require('./Backend/database.js')
 const 	thisURL		= "salon-on-backend.herokuapp.com"
-const 	testURL 	= "salon-on-thomas.herokuapp.com" //INSTEAD OF THIS, HOW ABOUT A HEROKU CONFIG VAR?/.ENV FOR LOCAL?
+const 	testURL 	= "salon-on-backend-test-thomas.herokuapp.com" //INSTEAD OF THIS, HOW ABOUT A HEROKU CONFIG VAR?/.ENV FOR LOCAL?
 
 //SERVER & SETUP
 async function start() {
@@ -15,7 +15,7 @@ async function start() {
 
 	// await database.disconnect();
 
-	setInterval(refresh, 300000); // every 5 minutes (300000)
+	setInterval(refresh, 3000); // every 5 minutes (300000)
 
 	//TODO Test
 	// TODO Consider CircleCI which is auto tests
@@ -29,7 +29,7 @@ async function refresh() {
 	// Thomas: I was getting errors unless this is sent via post request in this manner.
 	// If it worked for you Ethan, maybe we should talk about this.
 	options = {
-		hostname: thisURL,
+		hostname: testURL,
 		path: '/refresh',
 		method: 'POST'
 	};

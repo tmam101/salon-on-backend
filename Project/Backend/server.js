dc8fc7cc6938ae8048c1975940f43683f25bd0c8
Ethan Bateman <jetfire725@hotmail.com>
2019-10-15 16:51:59 -0400

    fixed type

--- Project/Backend/server.js
+++ Project/Backend/server.js
@@ -71,7 +71,7 @@ async function respond(response, value){
     let zip = info.zip;
     let radius = info.radius;
 
-    let profiles = await database.searchStylistLocation(parseint(zip), parseint(radius));
+    let profiles = await database.searchStylistLocation(parseInt(zip), parseInt(radius));
     await respond (this.res, profiles)
   }
 

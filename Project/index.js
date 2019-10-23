2ec56b94cda2e75ba38bada4f5a0a027662f2fa2
tmam101 <thomaspgoss98@gmail.com>
2019-10-23 08:22:35 -0400

    Update index.js

--- Project/index.js
+++ Project/index.js
@@ -9,7 +9,7 @@ async function start() {
 	// await database.connect()
 
 	// await database.disconnect();
-	let results = await database.SearchStylistsSpecificLocation('108 shadowood drive, chapel hill NC, 27514', 27514, 2);
+	let results = await database.searchStylistsSpecificLocation('108 shadowood drive, chapel hill NC, 27514', 27514, 2);
 	console.log(results);
 
 	setInterval(refresh, 300000); // every 5 minutes (300000)

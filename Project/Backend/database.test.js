3d1b21722c9572975fc265accf9e14462c4e900e
tmam101 <thomaspgoss98@gmail.com>
2019-10-24 17:59:46 -0400

    Update database.test.js

--- Project/Backend/database.test.js
+++ Project/Backend/database.test.js
@@ -16,6 +16,6 @@ describe ('getAllAmenities', function() {
     const amenities = await database.getAllAmenities()
     console.log(amenities);
     expect(amenities).toBeDefined()
-    expect(amenities.length)toBe("11")
+    expect(amenities.length).toBe("11")
   })
 })

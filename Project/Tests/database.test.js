c40ae90f61bd2c658d2acb043fa90b631e2cb0b1
tmam101 <thomaspgoss98@gmail.com>
2019-10-25 00:17:44 -0400

    Update format of tests

--- Project/Tests/database.test.js
+++ Project/Tests/database.test.js
@@ -1,19 +1,15 @@
+const	env = require('dotenv')
+env.config()
+const database = require('../Backend/database.js')
+
 describe ('getAllAmenities', function() {
-  const	env = require('dotenv')
-  env.config()
-  const database = require('../Backend/database.js')
-
-  it('should exist', async function() {
-    const amenities = await database.getAllAmenities()
-    expect(amenities).toBeDefined()
-  })
-
   it('should be a function', function() {
     expect(typeof database.getAllAmenities).toBe("function")
   })
 
   it('should be accurate', async function() {
     const amenities = await database.getAllAmenities()
+    expect(amenities).toBeDefined()
     expect(amenities.length).toBe(11)
     expect(amenities[0].name).toBe("Dryer")
   })

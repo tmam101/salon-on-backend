3ee8376bac9debfdffa94d5634d123b096adb557
tmam101 <thomaspgoss98@gmail.com>
2019-10-25 01:20:25 -0400

    Update database.test.js

--- Project/Tests/database.test.js
+++ Project/Tests/database.test.js
@@ -71,6 +71,6 @@ describe('searchByLocation', function() {
     const profiles = await database.searchByLocation(27514, 10)
     console.log(profiles)
     expect(profiles).toBeDefined()
-    expect(JSON.parse(profiles).length).toBeGreaterThan(0)
+    expect(JSON.parse(profiles).profiles.length).toBeGreaterThan(0)
   })
 })

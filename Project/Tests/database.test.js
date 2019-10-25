2348023957a16da284b759a6c722e99a6ebd7010
tmam101 <thomaspgoss98@gmail.com>
2019-10-25 00:23:03 -0400

    Update database.test.js

--- Project/Tests/database.test.js
+++ Project/Tests/database.test.js
@@ -23,5 +23,33 @@ describe('getAllHairStyles', function() {
     const hairstyles = await database.getAllHairStyles()
     console.log(hairstyles)
     expect(hairstyles).toBeDefined()
+    expect(hairstyles.length).toBe(10)
+    expect(hairstyles[0].name).toBe("Blow Out")
   })
 })
+
+describe('getAllStylists', function() {
+  it('should be a function', function() {
+    expect(typeof database.getAllStylists).toBe("function")
+  })
+  it('should be accurate', async function() {
+    const styles = await database.getAllStylists()
+    console.log(styles)
+    expect(styles).toBeDefined()
+    expect(styles.length).toBe(10)
+    expect(styles[0].name).toBe("Blow Out")
+  })
+})
+
+describe('getAllClients') {
+  it('should be a function', function() {
+    expect(typeof database.getAllClients).toBe("function")
+  })
+  it('should be accurate', async function() {
+    const clients = await database.getAllClients()
+    console.log(clients)
+    expect(clients).toBeDefined()
+    expect(clients.length).toBe(10)
+    expect(clients[0].name).toBe("Blow Out")
+  })
+}

569089b9a3f4f4c6610175dc7b8ebb7d2c55ca05
tmam101 <thomaspgoss98@gmail.com>
2019-11-05 13:59:47 -0500

    Remove index changes

--- Project/index.js
+++ Project/index.js
@@ -2,26 +2,18 @@
 const 	server		= require('./Backend/server.js')
 const		network 	= require('./Backend/network.js')
 const 	database  = require('./Backend/database.js')
-var refreshing = false
 //SERVER & SETUP
 //TODO Move things from devDependencies into dependencies?
 //TODO Remove node_modules from github
 async function start() {
 	await server.startServer();
-	setupRefresh()
+	setInterval(refresh, 300000); // every 5 minutes (300000)
 	// TODO set up review apps - these get created when you create a new pull request
 	// TODO Test sending multiple requests at once to see if the server can handle it.
 
 }
 
 start();
-
-function setupRefresh() {
-	if (!refreshing) {
-		setInterval(refresh, 300000); // every 5 minutes (300000)
-		refreshing = true
-	}
-}
 
 async function refresh() {
 	options = {
@@ -34,5 +26,3 @@ async function refresh() {
   };
 	network.post(options, body);
 }
-
-exports.start=start;
